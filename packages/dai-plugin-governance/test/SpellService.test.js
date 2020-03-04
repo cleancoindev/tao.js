c5c9a8a5c9995cad561e0408edaa380054777e37
shkfnly <ashoka.finley@gmail.com>
2020-03-04 10:10:23 -0500

    governance service working

--- packages/dai-plugin-governance/test/SpellService.test.js
+++ packages/dai-plugin-governance/test/SpellService.test.js
@@ -27,6 +27,7 @@ describe('use mainnet', () => {
   beforeAll(async () => {
     maker = await setupTestMakerInstance('mainnet');
     spellService = maker.service('spell');
+    jest.setTimeout(20000)
   });
 
   test('get spell execution date', async () => {
