ef2d9fce3cf81c76988bf24f1a611b6cb698b19e
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-05-18 08:45:32 -1000

    Update run-testchain.sh

--- scripts/run-testchain.sh
+++ scripts/run-testchain.sh
@@ -4,4 +4,4 @@ set -e
 CWD="${0%/*}"
 
 $CWD/set-polling-interval.sh
-$CWD/../node_modules/@makerdao/testchain/scripts/launch -s default --fast $@
+$CWD/../node_modules/@cleancoindev/testchain/scripts/launch -s default --fast $@
